5e85da647be62be6a4a9695e6b0b810a0a776929
prindarh <prindar@163.com>
2019-02-18 11:31:34 +0800

    hj

---
+++ static/config.js
@@ -0,0 +1,9 @@
+// export const BASEURL='http://192.168.20.145:8080/aobo'
+// export const BASEURL='http://192.168.21.31:8080/aobo'
+// export const BASEURL='http://192.168.20.153:8080/aobo'
+export const BASEURL='http://result.eolinker.com/LQQHzYze674abba7e9669b8cc69670c173da4e4cff65f15?uri='
+//  export const BASEURL='/aobo'
+export const isStatic=true
+export const BASEURLMOCK=''
+export const TIMEOUT=40000
+export const NETURL='http://tjhx.9cfcf.com/aobo/app/index.html'
