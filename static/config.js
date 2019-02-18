b206f6c6348c54f88113c5d419654d873bfe42de
prindarh <prindar@163.com>
2019-02-18 14:07:05 +0800

    hj

--- static/config.js
+++ static/config.js
@@ -1,7 +1,7 @@
 // export const BASEURL='http://192.168.20.145:8080/aobo'
 // export const BASEURL='http://192.168.21.31:8080/aobo'
 // export const BASEURL='http://192.168.20.153:8080/aobo'
-export const BASEURL='http://result.eolinker.com/LQQHzYze674abba7e9669b8cc69670c173da4e4cff65f15?uri='
+export const BASEURL='https://result.eolinker.com/LQQHzYze674abba7e9669b8cc69670c173da4e4cff65f15?uri='
 //  export const BASEURL='/aobo'
 export const isStatic=true
 export const BASEURLMOCK=''
